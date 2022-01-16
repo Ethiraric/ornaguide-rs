391ffa3fe98f1c284cf6f08f0db6a28ff0d5175e
Ethiraric <ethiraric@gmail.com>
2022-01-16 20:56:42 +0100

    Add Accessory parsing.

--- src/items.rs
+++ src/items.rs
@@ -17,10 +17,12 @@ pub enum Item {
     Weapon(Weapon),
     /// A legs item.
     Legs(Legs),
-    /// A Head item.
+    /// A head item.
     Head(Head),
-    /// A Material item.
+    /// A material item.
     Material(Material),
+    /// An accessory item.
+    Accessory(Accessory),
 }
 
 impl TryFrom<RawItem> for Item {
@@ -33,6 +35,7 @@ impl TryFrom<RawItem> for Item {
             "Legs" => Ok(Self::Legs(Legs::try_from(raw_item)?)),
             "Head" => Ok(Self::Head(Head::try_from(raw_item)?)),
             "Material" => Ok(Self::Material(Material::try_from(raw_item)?)),
+            "Accessory" => Ok(Self::Accessory(Accessory::try_from(raw_item)?)),
             _ => Err(Error::InvalidField(
                 String::from("Item"),
                 String::from("type"),
@@ -359,6 +362,83 @@ impl TryFrom<RawItem> for Material {
             dropped_by: item.dropped_by.unwrap_or_else(Vec::new),
             equipped_by: item.equipped_by.ok_or_else(missing_field("equipped_by"))?,
             quests: item.quests.unwrap_or_else(Vec::new),
+        })
+    }
+}
+
+/// An accessory item in Orna.
+pub struct Accessory {
+    pub name: String,
+    pub id: u32,
+    pub description: String,
+    pub tier: u32,
+    pub boss: bool,
+    pub arena: bool,
+    pub image: String,
+    pub stats: ItemStats,
+    pub element: Option<String>,
+    pub dropped_by: Vec<ItemDroppedBy>,
+    pub quests: Vec<ItemQuest>,
+    pub equipped_by: Vec<ItemEquippedBy>,
+    pub prevents: Vec<String>,
+    pub causes: Vec<String>,
+    pub cures: Vec<String>,
+    pub gives: Vec<String>,
+}
+
+impl TryFrom<RawItem> for Accessory {
+    type Error = Error;
+
+    /// Create an `Accessory` from a `RawItem`.
+    /// The `RawItem`'s `type` field must be `Accessory`.
+    fn try_from(item: RawItem) -> Result<Self, Self::Error> {
+        use Error::{InvalidField, MissingField};
+
+        if item.type_ != "Accessory" {
+            return Err(InvalidField(
+                String::from("Accessory"),
+                String::from("type"),
+                Some(item.type_),
+            ));
+        }
+
+        // Sanity check that an accessory is usable by all 3 classes.
+        if let Some(v) = &item.equipped_by {
+            if v.len() != 3 {
+                return Err(InvalidField(
+                    String::from("Accessory"),
+                    String::from("equipped_by[]"),
+                    Some(
+                        v.iter()
+                            .map(|equip| equip.name.clone())
+                            .collect::<Vec<_>>()
+                            .join(","),
+                    ),
+                ));
+            }
+        }
+
+        let missing_field = |field: &'static str| {
+            move || MissingField(String::from("Accessory"), String::from(field))
+        };
+
+        Ok(Self {
+            name: item.name,
+            id: item.id,
+            description: item.description,
+            tier: item.tier,
+            boss: item.boss,
+            arena: item.arena,
+            image: item.image,
+            stats: item.stats.ok_or_else(missing_field("stats"))?,
+            element: item.element,
+            dropped_by: item.dropped_by.unwrap_or_else(Vec::new),
+            quests: item.quests.unwrap_or_else(Vec::new),
+            equipped_by: item.equipped_by.unwrap_or_else(Vec::new),
+            prevents: item.prevents.unwrap_or_else(Vec::new),
+            causes: item.causes.unwrap_or_else(Vec::new),
+            cures: item.cures.unwrap_or_else(Vec::new),
+            gives: item.gives.unwrap_or_else(Vec::new),
         })
     }
 }
