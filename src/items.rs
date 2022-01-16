a2ff722c87810e7384f37732a7d06a8d3584a662
Ethiraric <ethiraric@gmail.com>
2022-01-16 19:13:29 +0100

    Add Legs parsing.

--- src/items.rs
+++ src/items.rs
@@ -15,6 +15,8 @@ pub enum Item {
     Armor(Armor),
     /// A weapon item.
     Weapon(Weapon),
+    /// A legs item.
+    Legs(Legs),
 }
 
 impl TryFrom<RawItem> for Item {
@@ -24,6 +26,7 @@ impl TryFrom<RawItem> for Item {
         match raw_item.type_.as_str() {
             "Armor" => Ok(Self::Armor(Armor::try_from(raw_item)?)),
             "Weapon" => Ok(Self::Weapon(Weapon::try_from(raw_item)?)),
+            "Legs" => Ok(Self::Legs(Legs::try_from(raw_item)?)),
             _ => Err(Error::InvalidField(
                 String::from("Item"),
                 String::from("type"),
@@ -159,6 +162,68 @@ impl TryFrom<RawItem> for Weapon {
             cures: item.cures.unwrap_or_else(Vec::new),
             gives: item.gives.unwrap_or_else(Vec::new),
             category: item.category.ok_or_else(missing_field("category"))?,
+        })
+    }
+}
+
+/// An legs item in Orna.
+pub struct Legs {
+    pub name: String,
+    pub id: u32,
+    pub description: String,
+    pub tier: u32,
+    pub boss: bool,
+    pub arena: bool,
+    pub image: String,
+    pub stats: ItemStats,
+    pub element: Option<String>,
+    pub materials: Vec<ItemMaterial>,
+    pub dropped_by: Vec<ItemDroppedBy>,
+    pub quests: Vec<ItemQuest>,
+    pub equipped_by: Vec<ItemEquippedBy>,
+    pub prevents: Vec<String>,
+    pub causes: Vec<String>,
+    pub cures: Vec<String>,
+    pub gives: Vec<String>,
+}
+
+impl TryFrom<RawItem> for Legs {
+    type Error = Error;
+
+    /// Create an `Legs` from a `RawItem`.
+    /// The `RawItem`'s `type` field must be `Legs`.
+    fn try_from(item: RawItem) -> Result<Self, Self::Error> {
+        use Error::{InvalidField, MissingField};
+
+        if item.type_ != "Legs" {
+            return Err(InvalidField(
+                String::from("Legs"),
+                String::from("type"),
+                Some(item.type_),
+            ));
+        }
+
+        let missing_field =
+            |field: &'static str| move || MissingField(String::from("Legs"), String::from(field));
+
+        Ok(Self {
+            name: item.name,
+            id: item.id,
+            description: item.description,
+            tier: item.tier,
+            boss: item.boss,
+            arena: item.arena,
+            image: item.image,
+            stats: item.stats.ok_or_else(missing_field("stats"))?,
+            element: item.element,
+            materials: item.materials.ok_or_else(missing_field("materials"))?,
+            dropped_by: item.dropped_by.unwrap_or_else(Vec::new),
+            quests: item.quests.unwrap_or_else(Vec::new),
+            equipped_by: item.equipped_by.unwrap_or_else(Vec::new),
+            prevents: item.prevents.unwrap_or_else(Vec::new),
+            causes: item.causes.unwrap_or_else(Vec::new),
+            cures: item.cures.unwrap_or_else(Vec::new),
+            gives: item.gives.unwrap_or_else(Vec::new),
         })
     }
 }
