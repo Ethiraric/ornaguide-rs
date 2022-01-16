4e13fcefa7f74c3ea2b1e966b4fae6309ea3c8d5
Ethiraric <ethiraric@gmail.com>
2022-01-16 21:59:43 +0100

    Add ItemItem parsing.

--- src/items.rs
+++ src/items.rs
@@ -25,6 +25,8 @@ pub enum Item {
     Accessory(AccessoryItem),
     /// An off-hand item.
     OffHand(OffHandItem),
+    /// An "item" item.
+    Item(ItemItem),
 }
 
 impl TryFrom<RawItem> for Item {
@@ -39,6 +41,7 @@ impl TryFrom<RawItem> for Item {
             "Material" => Ok(Self::Material(MaterialItem::try_from(raw_item)?)),
             "Accessory" => Ok(Self::Accessory(AccessoryItem::try_from(raw_item)?)),
             "Off-hand" => Ok(Self::OffHand(OffHandItem::try_from(raw_item)?)),
+            "Item" => Ok(Self::Item(ItemItem::try_from(raw_item)?)),
             _ => Err(Error::InvalidField(
                 String::from("Item"),
                 String::from("type"),
@@ -503,6 +506,70 @@ impl TryFrom<RawItem> for OffHandItem {
             causes: item.causes.unwrap_or_else(Vec::new),
             cures: item.cures.unwrap_or_else(Vec::new),
             gives: item.gives.unwrap_or_else(Vec::new),
+        })
+    }
+}
+
+/// An "item" item in Orna.
+pub struct ItemItem {
+    pub name: String,
+    pub id: u32,
+    pub description: String,
+    pub tier: u32,
+    pub boss: bool,
+    pub arena: bool,
+    pub image: String,
+    pub dropped_by: Vec<ItemDroppedBy>,
+    pub quests: Vec<ItemQuest>,
+    pub equipped_by: Vec<ItemEquippedBy>,
+}
+
+impl TryFrom<RawItem> for ItemItem {
+    type Error = Error;
+
+    /// Create an `Item` from a `RawItem`.
+    /// The `RawItem`'s `type` field must be `Item`.
+    fn try_from(item: RawItem) -> Result<Self, Self::Error> {
+        use Error::{InvalidField, MissingField};
+
+        if item.type_ != "Item" {
+            return Err(InvalidField(
+                String::from("Item"),
+                String::from("type"),
+                Some(item.type_),
+            ));
+        }
+
+        // Sanity check that an item is usable by all 3 classes.
+        if let Some(v) = &item.equipped_by {
+            if v.len() != 3 {
+                return Err(InvalidField(
+                    String::from("Item"),
+                    String::from("equipped_by[]"),
+                    Some(
+                        v.iter()
+                            .map(|equip| equip.name.clone())
+                            .collect::<Vec<_>>()
+                            .join(","),
+                    ),
+                ));
+            }
+        }
+
+        let missing_field =
+            |field: &'static str| move || MissingField(String::from("Item"), String::from(field));
+
+        Ok(Self {
+            name: item.name,
+            id: item.id,
+            description: item.description,
+            tier: item.tier,
+            boss: item.boss,
+            arena: item.arena,
+            image: item.image,
+            dropped_by: item.dropped_by.unwrap_or_else(Vec::new),
+            equipped_by: item.equipped_by.ok_or_else(missing_field("equipped_by"))?,
+            quests: item.quests.unwrap_or_else(Vec::new),
         })
     }
 }
