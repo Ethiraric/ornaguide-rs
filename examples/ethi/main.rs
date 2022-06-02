2e64c27014157557635319bf1394b88c2d29c416
Ethiraric <ethiraric@gmail.com>
2022-06-02 21:43:14 +0200

    Add OrnaData to ethi.

--- examples/ethi/main.rs
+++ examples/ethi/main.rs
@@ -17,7 +17,7 @@ use ornaguide_rs::{
     monsters::{MonsterDrop, RawMonster},
     skills::{RawSkill, Skill},
 };
-use output::generate_output_jsons;
+use output::{generate_output_jsons, OrnaData};
 
 use crate::codex::fetch::CodexItems;
 use crate::guide::fetch::AdminItems;
@@ -26,30 +26,21 @@ mod codex;
 mod guide;
 mod misc;
 mod output;
+
 #[allow(unused_variables)]
 #[allow(unused_mut)]
 fn ethi() -> Result<(), Error> {
     let _ = dotenv();
     let cookie = dotenv::var("ORNAGUIDE_COOKIE").unwrap();
-    let mut cache = CachedGuide::from_directory(Path::new("./jsons/"))?;
     let guide = OrnaAdminGuide::new(&cookie)?;
-    let mut items = cache.get_items().unwrap().to_vec();
-    let monsters = cache.get_monsters().unwrap();
-    let skills = cache.get_skills().unwrap();
-    // let static_ = guide.admin_retrieve_static_resources()?;
-
-    items.sort_unstable_by_key(|item| item.id);
+    let data = OrnaData::load_from("output")?;
 
     // codex::fetch::items(&guide)?;
     // codex::fetch::bosses(&guide)?;
     // codex::fetch::raids(&guide)?;
     // guide::fetch::items(&guide)?;
     // adorn_slots(&guide)?;
-    generate_output_jsons(&guide)?;
-
-    // guide.codex_fetch_monster_list()?
-    // guide.codex_fetch_skill_list()?;
-    // dbg!(guide.codex_fetch_item_list()?);
+    // generate_output_jsons(&guide)?;
 
     Ok(())
 }
--- examples/ethi/output.rs
+++ examples/ethi/output.rs
@@ -1,9 +1,17 @@
-use std::{fs::File, io::BufWriter};
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter},
+};
 
 use ornaguide_rs::{
     codex::Codex,
     error::Error,
-    guide::{AdminGuide, CachedGuide, OrnaAdminGuide},
+    guide::{AdminGuide, CachedGuide, OrnaAdminGuide, Static},
+};
+
+use crate::{
+    codex::fetch::{CodexBosses, CodexItems, CodexMonsters, CodexRaids},
+    guide::fetch::{AdminItems, AdminMonsters, AdminSkills},
 };
 
 pub fn generate_output_jsons(guide: &OrnaAdminGuide) -> Result<(), Error> {
@@ -83,4 +91,90 @@ pub fn generate_output_jsons(guide: &OrnaAdminGuide) -> Result<(), Error> {
     )?;
 
     Ok(())
+}
+
+pub struct CodexData {
+    pub items: CodexItems,
+    pub raids: CodexRaids,
+    pub monsters: CodexMonsters,
+    pub bosses: CodexBosses,
+}
+
+pub struct GuideData {
+    pub items: AdminItems,
+    pub monsters: AdminMonsters,
+    pub skills: AdminSkills,
+    pub static_: Static,
+}
+
+pub struct OrnaData {
+    pub codex: CodexData,
+    pub guide: GuideData,
+}
+
+impl OrnaData {
+    pub fn load_from(directory: &str) -> Result<Self, Error> {
+        Ok(OrnaData {
+            codex: CodexData {
+                items: serde_json::from_reader(BufReader::new(File::open(format!(
+                    "{}/codex_items.json",
+                    directory
+                ))?))?,
+                raids: serde_json::from_reader(BufReader::new(File::open(format!(
+                    "{}/codex_raids.json",
+                    directory
+                ))?))?,
+                monsters: serde_json::from_reader(BufReader::new(File::open(format!(
+                    "{}/codex_monsters.json",
+                    directory
+                ))?))?,
+                bosses: serde_json::from_reader(BufReader::new(File::open(format!(
+                    "{}/codex_bosses.json",
+                    directory
+                ))?))?,
+            },
+            guide: GuideData {
+                items: serde_json::from_reader(BufReader::new(File::open(format!(
+                    "{}/guide_items.json",
+                    directory
+                ))?))?,
+                monsters: serde_json::from_reader(BufReader::new(File::open(format!(
+                    "{}/guide_monsters.json",
+                    directory
+                ))?))?,
+                skills: serde_json::from_reader(BufReader::new(File::open(format!(
+                    "{}/guide_skills.json",
+                    directory
+                ))?))?,
+                static_: Static {
+                    spawns: serde_json::from_reader(BufReader::new(File::open(format!(
+                        "{}/guide_spawns.json",
+                        directory
+                    ))?))?,
+                    elements: serde_json::from_reader(BufReader::new(File::open(format!(
+                        "{}/guide_elements.json",
+                        directory
+                    ))?))?,
+                    item_types: serde_json::from_reader(BufReader::new(File::open(format!(
+                        "{}/guide_item_types.json",
+                        directory
+                    ))?))?,
+                    equipped_bys: serde_json::from_reader(BufReader::new(File::open(format!(
+                        "{}/guide_equipped_bys.json",
+                        directory
+                    ))?))?,
+                    status_effects: serde_json::from_reader(BufReader::new(File::open(format!(
+                        "{}/guide_status_effects.json",
+                        directory
+                    ))?))?,
+                    item_categories: serde_json::from_reader(BufReader::new(File::open(
+                        format!("{}/guide_item_categories.json", directory),
+                    )?))?,
+                    monster_families: serde_json::from_reader(BufReader::new(File::open(
+                        format!("{}/guide_monster_families.json", directory),
+                    )?))?,
+                },
+            },
+        })
+    }
 }
